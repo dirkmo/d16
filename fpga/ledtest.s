4c0138e72427e5db92821a3301fcf2abf27b0eaf
Dirk Moeller <moeller.dirk80@gmail.com>
2018-10-23 21:44:23 +0200

    ledtest.s with ram access, added testbench

--- fpga/ledtest.s
+++ fpga/ledtest.s
@@ -1,11 +1,15 @@
 .org 0
 .equ ledport 0x7000
 
-1
+.equ ram 0x1000
 
-loop:   dup set braz
+1 ram store
+
+loop:   ram load
+        dup set braz
         dup ledport store
         1 lsl
+        ram store
         loop jmp
 
 set:    drop 1
--- fpga/top.v
+++ fpga/top.v
@@ -3,16 +3,28 @@
 module top(
     clk_50mhz,
     i_reset,
-    o_addr,
-    dat,
+    
+    sram_addr,
+    sram0_data,
+    sram_oen,
+    sram_wen,
+    sram0_cen,
+    sram0_ubn,
+    sram0_lbn,
     
     leds
 );
 
 input clk_50mhz;
 input i_reset;
-output [15:0] o_addr;
-inout dat;
+
+output [15:0] sram_addr;
+inout  [15:0] sram0_data;
+output sram_oen;
+output sram_wen;
+output sram0_cen;
+output sram0_ubn;
+output sram0_lbn;
 
 output reg [7:0] leds;
 
@@ -22,12 +34,27 @@ reg  [15:0] i_dat_cpu;
 wire we_cpu;
 wire cyc_cpu;
 
+assign sram_addr = addr_cpu;
+assign sram_oen = we_cpu;
+assign sram_wen = ~we_cpu;
+assign sram0_ubn = 1'b0;
+assign sram0_lbn = 1'b0;
+assign sram0_cen = ~cyc_cpu;
+
+assign sram0_data = we_cpu ? o_dat_cpu : 15'hz;
+
+//`define SLOW
+
+`ifdef SLOW
 reg [21:0] clk_div;
 wire clk = clk_div[21];
 always @(posedge clk_50mhz)
 begin
     clk_div <= clk_div + 1;
 end
+`else
+wire clk = clk_50mhz;
+`endif
 
 d16 cpu (
     .i_clk(clk), 
@@ -41,16 +68,12 @@ d16 cpu (
     );
 
 
-reg [15:0] ram[0:127];
-
 // writing
 always @(posedge clk)
 begin
     if(cyc_cpu && we_cpu) begin
         if( addr_cpu == 16'h7000 ) begin
             leds <= o_dat_cpu;
-        end else if( addr_cpu >= 16'h1000 && addr_cpu < 16'h1080 ) begin
-            ram[addr_cpu[6:0]] <= o_dat_cpu[15:0];
         end
     end
     if( i_reset ) begin
@@ -61,15 +84,10 @@ end
 // reading
 always @(addr_cpu)
 begin
-    if( addr_cpu < 16'h400 ) begin
-        case( addr_cpu )
-            //i_dat_cpu[15:0] = 16'h0000;
+    case( addr_cpu )
 `include "ledtest.v"
-            default: i_dat_cpu[15:0] = 16'h0000;
-        endcase
-    end else if( addr_cpu >= 16'h1000 && addr_cpu < 16'h1080 ) begin
-        i_dat_cpu[15:0] = ram[addr_cpu[6:0]];
-    end
+        default: i_dat_cpu[15:0] = sram0_data[15:0];
+    endcase
 end
 
 
