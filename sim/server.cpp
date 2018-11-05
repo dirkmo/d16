c79c70b15e4c031e636dbb33f89043469afffd4c
Dirk Möller <dirk.moeller@dreamchip.de>
2018-11-05 08:10:30 +0100

    threaded server

--- sim/server.cpp
+++ sim/server.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <unistd.h>
 #include <iostream>
+#include <thread>
 #include "server.h"
 
 using namespace std;
@@ -20,6 +21,8 @@ UDSServer::UDSServer( std::string fn ) {
         throw runtime_error("ERROR: Cannot bind to socket");
     }
     listen( m_socket_fd, 0 );
+    m_thread = thread( [=] { threadfunc(); } );
+    m_bRunning = true;
 }
 
 UDSServer::~UDSServer() {
@@ -31,39 +34,42 @@ UDSServer::~UDSServer() {
     }
 }
 
-void UDSServer::wait() {
-    socklen_t addrlen;
-    m_client_fd = accept ( m_socket_fd, (struct sockaddr *) &m_sockaddr, &addrlen );
+void UDSServer::threadfunc() {
+    uint8_t buf[128];
+    while(m_bRunning) {
+        socklen_t addrlen;
+        m_client_fd = accept ( m_socket_fd, (struct sockaddr *) &m_sockaddr, &addrlen );
+        while( m_client_fd > -1 ) {
+            ssize_t size = recv( m_client_fd, buf, sizeof(buf), 0 );
+            if( size > 0 ) {
+                for( auto d: buf ) {
+                    m_vRecData.push_back(d);
+                }
+            }
+        }
+    }
 }
+
 
 bool UDSServer::send( const std::vector<uint8_t> data ) {
     if( m_client_fd > -1 ) {
-        return ::send (m_client_fd, data.data(), data.size(), 0) == data.size();
+        return ::send( m_client_fd, data.data(), data.size(), 0 ) == static_cast<ssize_t>(data.size());
     }
     return false;
 }
 
 bool UDSServer::receive( std::vector<uint8_t>& data ) {
-    bool rec = false;
-    if( m_client_fd > -1 ) {
-        uint8_t buf[64];
-        ssize_t size;
-        data.clear();
-        while( (size = recv( m_client_fd, &buf, sizeof(buf), MSG_DONTWAIT ) ) > 0 ) {
-            for( int i = 0 ; i < size; i++ ) {
-                data.push_back(buf[i]);
-            }
-            rec |= size > 0;
-        }
+    if( m_vRecData.size() ) {
+        data = m_vRecData;
+        return true;
     }
-    return rec;
+    return false;
 }
 
 int main() {
     cout << "d16sim console" << endl;
     UDSServer uds("/tmp/d16sim.uds");
     cout << "Waiting for connection to simulator...";
-    uds.wait();
     cout << "connected." << endl;
     vector<uint8_t> data;
     while(1) {
--- sim/server.h
+++ sim/server.h
@@ -3,20 +3,25 @@
 
 #include <string>
 #include <vector>
+#include <thread>
 
 class UDSServer {
 public:
     UDSServer( std::string fn );
     ~UDSServer();
     
-    void wait();
     bool send( const std::vector<uint8_t> data );
     bool receive( std::vector<uint8_t>& data );
 
 private:
+    void threadfunc();
+
     int m_socket_fd = -1;
     int m_client_fd = -1;
     struct sockaddr_un m_sockaddr;
+    std::thread m_thread;
+    bool m_bRunning = false;
+    std::vector<uint8_t> m_vRecData;
 };
 
 #endif
